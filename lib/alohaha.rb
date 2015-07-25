467140d71daa97ce94fe440f11cdc9fa5efbafe0
jiunjiun <quietmes@gmail.com>
2015-07-26 03:48:35 +0800

    Add method

--- lib/alohaha.rb
+++ lib/alohaha.rb
@@ -25,5 +25,21 @@ class Alohaha
   def departure
     @flights.select {|flights| flights.kind == 'Departure' }
   end
+
+  def yesterday
+    @flights.select {|flights| flights.datetime > Date.today.prev_day && flights.datetime < Date.today }
+  end
+
+  def today
+    @flights.select {|flights| flights.datetime > Date.today && flights.datetime < Date.today.next_day }
+  end
+
+  def tomorrow
+    @flights.select {|flights| flights.datetime > Date.today.next_day }
+  end
+
+  def by_datetime(datetime = DateTime.now)
+    @flights.select {|flights| flights.datetime > datetime }
+  end
 end
 
